9189bdd2cae4c114afe61f5573215976ef106465
丘伟康 <642827353@qq.com>
2022-08-27 17:04:57 +0800

    feat: useQuery

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,12 +1,18 @@
 import { createSelectorQuery, NodesRef, SelectorQuery } from '@tarojs/taro'
+import { TaroElement } from '@tarojs/runtime'
 import { useCallback, useRef } from 'react'
 
 export type SelectorMethod = {
   getBoundingClientRect: getBoundingClientRectType
+  getRefSize: getRefSizeType
 }
 
 export type getBoundingClientRectType = (
   selector: string,
+) => Promise<NodesRef.BoundingClientRectCallbackResult>
+
+export type getRefSizeType = (
+  element: TaroElement,
 ) => Promise<NodesRef.BoundingClientRectCallbackResult>
 
 function useQuery(): [SelectorQuery, SelectorMethod] {
@@ -37,10 +43,29 @@ function useQuery(): [SelectorQuery, SelectorMethod] {
     [querySelector],
   )
 
+  const getRefSize = useCallback<getRefSizeType>(
+    (element: TaroElement) => {
+      return new Promise((resolve, reject) => {
+        if (!element) {
+          reject({})
+        } else {
+          try {
+            let selectorQuery = querySelector('#' + element.id).boundingClientRect(resolve)
+            selectorQuery.exec()
+          } catch (e) {
+            reject(e)
+          }
+        }
+      })
+    },
+    [querySelector],
+  )
+
   return [
     query.current,
     {
       getBoundingClientRect,
+      getRefSize,
     },
   ]
 }
