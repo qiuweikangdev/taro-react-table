e3b056d0ba8410b59bcc7998a5d0bc564f501a7b
丘伟康 <642827353@qq.com>
2022-08-27 17:01:29 +0800

    fix: 更新demo

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -6,67 +6,67 @@ export default function Demo() {
   const [loading, setLoading] = useState(false)
   const [dataSource, setDataSource] = useState([
     {
-      name1: '无人之境1',
-      name2: '打回原形1',
-      name3: '防不胜防1',
-      name4: '十面埋伏1',
+      song1: '无人之境1',
+      song2: '打回原形1',
+      song3: '防不胜防1',
+      song4: '十面埋伏1',
     },
     {
-      name1: '无人之境2',
-      name2: '打回原形2',
-      name3: '防不胜防2',
-      name4: '十面埋伏2',
+      song1: '无人之境2',
+      song2: '打回原形2',
+      song3: '防不胜防2',
+      song4: '十面埋伏2',
     },
     {
-      name1: '无人之境3',
-      name2: '打回原形3',
-      name3: '防不胜防3',
-      name4: '十面埋伏3',
+      song1: '无人之境3',
+      song2: '打回原形3',
+      song3: '防不胜防3',
+      song4: '十面埋伏3',
     },
     {
-      name1: '无人之境4',
-      name2: '打回原形4',
-      name3: '防不胜防4',
-      name4: '十面埋伏4',
+      song1: '无人之境4',
+      song2: '打回原形4',
+      song3: '防不胜防4',
+      song4: '十面埋伏4',
     },
     {
-      name1: '无人之境5',
-      name2: '打回原形5',
-      name3: '防不胜防5',
-      name4: '十面埋伏5',
+      song1: '无人之境5',
+      song2: '打回原形5',
+      song3: '防不胜防5',
+      song4: '十面埋伏5',
     },
     {
-      name1: '无人之境6',
-      name2: '打回原形6',
-      name3: '防不胜防6',
-      name4: '十面埋伏6',
+      song1: '无人之境6',
+      song2: '打回原形6',
+      song3: '防不胜防6',
+      song4: '十面埋伏6',
     },
   ])
   const [loadStatus, setLoadStatus] = useState<LoadStatus>(null)
   const [sortInfo, setSortInfo] = useState<Omit<SorterEvent, 'column'>>({
-    field: 'name1',
+    field: 'song1',
     order: 'ascend',
   })
 
   const columns: Columns[] = [
     {
-      title: '陈奕迅1',
-      dataIndex: 'name1',
+      title: 'Song1',
+      dataIndex: 'song1',
       sorter: true,
       fixed: 'left',
-      sortOrder: sortInfo.field == 'name1' && sortInfo.order,
+      sortOrder: sortInfo.field == 'song1' && sortInfo.order,
     },
     {
-      title: '陈奕迅2',
-      dataIndex: 'name2',
+      title: 'Song2',
+      dataIndex: 'song2',
     },
     {
-      title: '陈奕迅3',
-      dataIndex: 'name3',
+      title: 'Song3',
+      dataIndex: 'song3',
     },
     {
-      title: '陈奕迅4',
-      dataIndex: 'name4',
+      title: 'Song4',
+      dataIndex: 'song4',
     },
   ]
 
@@ -77,10 +77,10 @@ export default function Demo() {
         for (let i = 1; i < 10; i++) {
           const size = list.length + 1
           list.push({
-            name1: `无人之境${size}`,
-            name2: `打回原形${size}`,
-            name3: `防不胜防${size}`,
-            name4: `十面埋伏${size}`,
+            song1: `无人之境${size}`,
+            song2: `打回原形${size}`,
+            song3: `防不胜防${size}`,
+            song4: `十面埋伏${size}`,
           })
         }
         resolve(list)
@@ -89,6 +89,8 @@ export default function Demo() {
   }
 
   const onLoad = async e => {
+    console.log('load')
+
     setLoadStatus('loading')
     const list = await getList()
     setDataSource(list)
