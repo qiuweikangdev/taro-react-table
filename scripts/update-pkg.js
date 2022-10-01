19e12bb8c0b60b6b72cadaf0f3a54e6af39c294d
丘伟康 <642827353@qq.com>
2022-10-01 15:59:05 +0800

    feat: update-pkg

---
+++ scripts/update-pkg.js
@@ -0,0 +1,21 @@
+// update echarts package.json
+
+const pkg = require('../packages/table/package.json')
+const { writeFileSync } = require('fs')
+const path = require('path')
+if (process.env.NODE_ENV === 'prod') {
+  Object.assign(pkg, {
+    main: 'dist/index.js',
+    module: 'dist/index.esm.js',
+  })
+} else {
+  Object.assign(pkg, {
+    main: 'src/index.ts',
+    module: 'src/index.ts',
+  })
+}
+
+writeFileSync(
+  path.resolve(process.cwd(), 'packages/table/package.json'),
+  JSON.stringify(pkg, null, 2),
+)
